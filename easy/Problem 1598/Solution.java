814d6bb61bd14a6900037c1aa3549172c2d25d13
Abdul Hadi <117abdulhadi@gmail.com>
2025-03-28 15:36:29 +0500

    Crawler Log Folder

--- easy/Problem 1598/Solution.java
+++ easy/Problem 1598/Solution.java
@@ -1,10 +1,18 @@
 class Solution {
+    /*
+     * Calculates the minimum number of operations required to traverse a file system represented by an array of logs.
+     @ param logs An array of strings representing the logs.
+     $ return The minimum number of operations required.
+     */
     public int minOperations(String[] logs) {
         int level = 0;
+        // Iterate through each log
         for (String log : logs){
-            if (log.equals("../")){
+            if (log.equals("../")) {
+                // Move up one level
                 level = Math.max(0,level-1);
-            }else if (!log.equals("./")){
+            } else if (!log.equals("./")) {
+                // Move down one level
                 level += 1;
             }
         }
